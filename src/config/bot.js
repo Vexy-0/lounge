5638107de52cca67d6a56fefd51f41c9fde17a5c
Vexy-0 <romanchohan0@gmail.com>
2026-08-08 01:15:17 +0500

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -25,7 +25,7 @@ export const botConfig = {
       {
         name: "The lounge", // required by Discord API, not shown in the client
         state: "Watching",     // this is what people actually see
-        type: 4,               // Custom
+        type: 3,               // Custom
       },
     ],
   },
